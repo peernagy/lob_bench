0979974028452da754aca0ee738b97349abfea47
saschafrey <saschayvesfrey@gmail.com>
2024-05-20 18:38:43 +0100

    impact filtering done, by order size/side and time.

--- impact.py
+++ impact.py
@@ -4,8 +4,57 @@ Functions to estimate market impact of different events produced by the model, a
 from typing import Optional
 import numpy as np
 import pandas as pd
+from decimal import Decimal
+from typing import Tuple
+
+def filter_sequence_for_impact(message_sequence : pd.DataFrame,
+                                book_sequence: pd.DataFrame,
+                                ordersize: Tuple[int,int],
+                                orderside: int,
+                                timestamp :Tuple[str,str] = None,
+                                sequence_length:int=100,):
+    """Given an input sequence of LOBSTER messages and L2 orderbook data, of undetermined length. 
+    Aims to find specific impact events, as defined by: order quantity, order side, limit or execution, potentially time.
+
+            Parameters:
+            message_sequence : Messages from a single day in a DF
+            book_sequence: L2 Book states from a single day in a DF
+            ordersize: Tuple with min and max order size (quant) to consider for impact orders (min,max)
+            orderside: int,
+            timestamps :Optional Tuple with min and max time to consider for impact orders (min,max)
+            timestamp_below: str = None,
+            sequence_length:int=100,
+                
+        Returns:
+                Messages that follow an impact event. 
+                Book states that follow an impact event. 
+    
+    """
+    if timestamp is None:
+        impact_msgs=message_sequence.loc[(message_sequence['size']>ordersize[0])
+                                         &(message_sequence['size']<ordersize[1])
+                                         &(message_sequence['direction']==orderside)]
+    else:
+        message_sequence['time']=message_sequence['time'].map(Decimal)
+        print((message_sequence['time']<=Decimal(timestamp[1]))
+                & (message_sequence['time']>=Decimal(timestamp[0])))
+        impact_msgs=message_sequence.loc[(message_sequence['size']>=ordersize[0])
+                                        &(message_sequence['size']<=ordersize[1])
+                                        & (message_sequence['direction']==orderside)
+                                        & (message_sequence['time']<=Decimal(timestamp[1]))
+                                        & (message_sequence['time']>=Decimal(timestamp[0]))]
+    sequence_indices=[]
+    for i in impact_msgs.index:
+        if (i+sequence_length) > len(message_sequence):
+            break
+        sequence_indices=sequence_indices+list(range(i,i+sequence_length,1))
+    filtered_messages=message_sequence.loc[sequence_indices]
+    filtered_books=book_sequence.loc[sequence_indices]
+    return filtered_messages,filtered_books
 
 
+
+ 
 def get_impact_message(
         price: int,
         is_buy_message: bool,
